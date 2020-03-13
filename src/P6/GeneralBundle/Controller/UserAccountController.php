a9c7fef5ea314547c766248b1fa1b42dc560829f
Quentin <quentin.civiale@gmail.com>
2020-03-13 11:15:53 +0100

    Gestion des comptes users + edit du compte saisonnier

--- src/P6/GeneralBundle/Controller/UserAccountController.php
+++ src/P6/GeneralBundle/Controller/UserAccountController.php
@@ -2,30 +2,121 @@
 
 namespace P6\GeneralBundle\Controller;
 
+use P6\GeneralBundle\Entity\Advert;
 use P6\GeneralBundle\Entity\Employer;
 use P6\GeneralBundle\Entity\Seasonal;
 use P6\GeneralBundle\Entity\User;
+use P6\GeneralBundle\Form\editSeasonalAccountType;
+use P6\GeneralBundle\Helpers\AdvertHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\PathPackage;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
+use Symfony\Component\Asset\VersionStrategy\StaticVersionStrategy;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserAccountController extends Controller
 {
-    public function employerAccountAction(Employer $employer)
-    {
-        $employerRepository = $this->getDoctrine()->getRepository('GeneralBundle:Employer');
-        $employer = $employerRepository->findOneBy(['id' => $employer->getId()]);
+//    public function employerAccountAction(Employer $employer)
+//    {
+//        $employerRepository = $this->getDoctrine()->getRepository('GeneralBundle:Employer');
+//        $employer = $employerRepository->findOneBy(['id' => $employer->getId()]);
+//
+//        return $this->render('@General/Default/employerAccount.html.twig', [
+//            'employer' => $employer,
+//        ]);
+//    }
+//
+//    public function seasonalAccountAction(Seasonal $seasonal)
+//    {
+//        $seasonalRepository = $this->getDoctrine()->getRepository('GeneralBundle:Seasonal');
+//        $seasonal = $seasonalRepository->findOneBy(['id' => $seasonal->getId()]);
+//
+//        return $this->render('@General/Default/seasonalAccount.html.twig', [
+//            'seasonal' => $seasonal,
+//        ]);
+//    }
 
-        return $this->render('@General/Default/employerAccount.html.twig', [
-            'employer' => $employer,
+//    public function userAccountAction()
+//    {
+//        return $this->render('@General/Default/userAccount.html.twig');
+//    }
+
+    public function userProfilAction($id, User $user)
+    {
+        $userInfo = $this->getDoctrine()->getRepository('GeneralBundle:User')->find($id);
+        $advertsNumber = $this->getDoctrine()->getRepository('GeneralBundle:Advert')->getAdvertNumberByUser($user);
+
+        return $this->render('@General/Default/userAccount.html.twig', [
+            'user' => $userInfo,
+            'advertNumber' => $advertsNumber
         ]);
     }
 
-    public function seasonalAccountAction(Seasonal $seasonal)
+    public function editSeasonalAccountAction(Seasonal $seasonal, Request $request, User $user)
     {
-        $seasonalRepository = $this->getDoctrine()->getRepository('GeneralBundle:Seasonal');
-        $seasonal = $seasonalRepository->findOneBy(['id' => $seasonal->getId()]);
+        $advertsNumber = $this->getDoctrine()->getRepository('GeneralBundle:Advert')->getAdvertNumberByUser($user);
 
-        return $this->render('@General/Default/seasonalAccount.html.twig', [
+        $editSeasonalForm = $this->createForm(editSeasonalAccountType::class, $seasonal);
+
+        $oldProfilPicture = $seasonal->getProfilPicture();
+
+        $editSeasonalForm->handleRequest($request);
+
+        if ($editSeasonalForm->isSubmitted() && $editSeasonalForm->isValid()) {
+            /** @var $seasonal Seasonal */
+            $seasonal = $editSeasonalForm->getData();
+
+            // Gestion de la mise en place de la nouvelle image de profil du saisonnier
+
+            // récupération de l'image via son url si une nouvelle image est proposée
+            $file = $editSeasonalForm->get('profilPicture')->getData();
+            if (!empty($file)) {
+                $filename =  pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $filename.'.'.$file->guessExtension();
+                $newFilename = str_replace(' ', '', $newFilename);
+
+//                try {
+                $file->move(
+                    $this->getParameter('profil_picture_directory'),
+                    $newFilename
+                );
+//                } catch (FileException $e) {
+//                    dump($e);
+//                }
+
+                $finalPath = 'uploads/profilPicture/'.$newFilename;
+                $seasonal->setProfilPicture($finalPath);
+            }
+
+            else {
+                // condition lorsque l'image de profil n'est pas modifiée
+                if (!empty($oldProfilPicture)) {
+                    $seasonal->setProfilPicture($oldProfilPicture);
+                }
+            }
+
+//            $seasonal->setUpdatedAt(new \DateTime());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($seasonal);
+            $em->flush();
+
+            $this->addFlash('editSeasonalAccount', 'Votre image de profil a été modifié avec succès !');
+
+            return $this->render('@General/Default/userAccount.html.twig', [
+                'advertNumber' => $advertsNumber
+            ]);
+        }
+
+        return $this->render('@General/Default/editUserAccount.html.twig', [
             'seasonal' => $seasonal,
+            'file' => $oldProfilPicture,
+            'editSeasonalForm' => $editSeasonalForm->createView(),
         ]);
     }
 
---
+++ src/P6/GeneralBundle/Form/editSeasonalAccountType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace P6\GeneralBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class editSeasonalAccountType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('profilPicture', FileType::class, ['required' => false, 'data_class' => null])
+            ->add('resumeLink', UrlType::class)
+            ->add('email', EmailType::class)
+            ->getForm();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'P6\GeneralBundle\Entity\Seasonal',
+            'validation_groups' => ['Default', 'Registration'],
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'p6_generalbundle_seasonal';
+    }
+
+
+}
