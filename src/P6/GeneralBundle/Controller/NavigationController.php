2022f7ef14b7f9594e0774ec599f91e99a4debec
Quentin <quentin.civiale@gmail.com>
2020-03-13 11:38:22 +0100

    Ajout de code pour afficher les adverts dans la homepage

--- src/P6/GeneralBundle/Controller/NavigationController.php
+++ src/P6/GeneralBundle/Controller/NavigationController.php
@@ -2,12 +2,19 @@
 
 namespace P6\GeneralBundle\Controller;
 
+use P6\GeneralBundle\Entity\AdvertSearch;
+use P6\GeneralBundle\Form\AdvertSearchType;
+use P6\GeneralBundle\Repository\AdvertRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class NavigationController extends Controller
 {
     public function homeAction()
     {
-        return $this->render('@General/Default/index.html.twig');
+        $advert = $this->getDoctrine()->getRepository('GeneralBundle:Advert')->findLatestAdvert();
+
+        return $this->render('@General/Default/index.html.twig', [
+            'adverts' => $advert
+        ]);
     }
 }
